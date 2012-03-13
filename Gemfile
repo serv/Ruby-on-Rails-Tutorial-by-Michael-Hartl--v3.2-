54686e29b24bebe65d9e3b7b6afcee3a7e50d0a7
Jason Kim <jasonkim@Jasons-MacBook-Pro.local>
2012-03-12 21:51:22 -0400

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -2,11 +2,13 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.2'
 gem 'bootstrap-sass', '2.0.0'
+gem 'bcrypt-ruby', '3.0.1'
 
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.8.1'
+  gem 'annotate', '~> 2.4.1.beta'
 end
 
 # Gems used only for assets and not required
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,7 +6,7 @@ describe "User pages" do
   describe "signup page" do
     before { visit signup_path }
     
-    it { should have_selector('h1',     text: 'Sign Up') }
+    it { should have_selector('h1',     text: 'Sign up') }
     it { should have_selector('title',  text: full_title('Sign up')) }
   end
 end
