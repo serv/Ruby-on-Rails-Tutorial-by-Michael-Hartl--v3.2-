518bd0290a1e243cdeec579e4265928a1891c104
Jason Kim <jasonkim@Jasons-MacBook-Pro.local>
2012-03-16 15:45:43 -0400

    Finish sign in

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,3 +6,15 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
